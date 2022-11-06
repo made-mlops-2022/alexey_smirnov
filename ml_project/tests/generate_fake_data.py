8a130bff2a631c260fe696574c9d0b16a7959644
Alexey Smirnov <ziiggy13@gmail.com>
2022-11-06 04:18:32 +0300

    add test data generator

---
+++ ml_project/tests/generate_fake_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sdv.tabular import GaussianCopula
+
+
+def generate_fake_data():
+    data = pd.read_csv('../data/raw/heart_cleveland_upload.csv')
+    model = GaussianCopula()
+    model.fit(data)
+    fake_data = model.sample(10000)
+    fake_data.to_csv('fake_data/fake_data.csv', index=False)
