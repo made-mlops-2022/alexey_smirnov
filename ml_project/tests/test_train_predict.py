cd623289e0c7854f7d1573887e3d01a78c8c866a
Alexey Smirnov <ziiggy13@gmail.com>
2022-11-06 22:53:19 +0300

    add train pred tests

---
+++ ml_project/tests/test_train_predict.py
@@ -0,0 +1,61 @@
+import os
+import logging
+import pickle
+import shutil
+import sys
+import unittest
+
+from hydra import compose, initialize
+from hydra.utils import instantiate
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression
+from src.train import run_train_pipeline
+from src.predict import run_predict_pipeline
+
+PROJECT_PATH = os.getcwd()
+SOURCE_PATH = os.path.join(
+    PROJECT_PATH, 'src'
+)
+sys.path.append(SOURCE_PATH)
+
+logging.disable(logging.CRITICAL)
+
+
+class TestTrainPredict(unittest.TestCase):
+    def test_train(self):
+        os.makedirs('tests/models', exist_ok=True)
+
+        with initialize(version_base=None, config_path='../configs'):
+            params = compose(config_name="config")
+        # params = instantiate(params, _convert_='partial')
+
+        run_train_pipeline(params)
+
+        self.assertTrue(os.path.exists(params.test_data_path))
+        self.assertTrue(os.path.exists(params.model.model_path))
+        self.assertTrue(os.path.exists(params.model.metric_json_path))
+        self.assertTrue(os.path.exists(params.model.processed_data_path))
+        self.assertTrue(os.path.exists(params.model.transformer_model_path))
+
+        with open(params.model.model_path, 'rb') as file:
+            model = pickle.load(file)
+        self.assertIsInstance(model, LogisticRegression)
+
+        with open(params.model.transformer_model_path, 'rb') as f:
+            model = pickle.load(f)
+        self.assertIsInstance(model, ColumnTransformer)
+
+    def test_predict(self):
+        with initialize(version_base=None, config_path='../configs'):
+            params = compose(config_name="config")
+        params = instantiate(params, _convert_='partial')
+        run_predict_pipeline.callback(model_path=params.model.model_path,
+                                      transformer_path=params.model.transformer_model_path,
+                                      test_data_path=params.test_data_path,
+                                      predictions_path='models/prediction.csv')
+        self.assertTrue(os.path.exists('tests/models/prediction.csv'))
+        shutil.rmtree('tests/models')
+
+
+if __name__ == '__main__':
+    unittest.main()
