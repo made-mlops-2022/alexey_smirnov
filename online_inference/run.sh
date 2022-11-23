e07afeed21778ac9cb4c6089cbac7efe34238133
Alexey Smirnov <ziiggy13@gmail.com>
2022-11-24 00:17:49 +0300

    fix gdown

--- online_inference/run.sh
+++ online_inference/run.sh
@@ -10,16 +10,16 @@ fi
 
 if [[ ! -f $MODEL_PATH ]]
 then
-    gdown https://drive.google.com/file/d/1xPq7zv8cH0lVfKPOflhEYuoYGVU37yi-/view?usp=sharing --output=$MODEL_PATH
+    gdown https://drive.google.com/file/d/1xPq7zv8cH0lVfKPOflhEYuoYGVU37yi-/view?usp=sharing --fuzzy --output=$MODEL_PATH
 else
     echo "model already exists"
 fi
 
 if [[ ! -f $TRANSFORMER_PATH ]]
 then
-    gdown https://drive.google.com/file/d/1Vsa6G9dqbUnZ6QPwU3bnQphot287-jxX/view?usp=sharing --output=$TRANSFORMER_PATH
+    gdown https://drive.google.com/file/d/1Vsa6G9dqbUnZ6QPwU3bnQphot287-jxX/view?usp=sharing --fuzzy --output=$TRANSFORMER_PATH
 else 
-    echo "transformer aldeady exists"
+    echo "transformer already exists"
 fi
 
 uvicorn main:app --reload --host 0.0.0.0 --port 8000
