a9128b3afdaa5979000f6d936ec71d56296dd351
Alexey Smirnov <ziiggy13@gmail.com>
2022-11-23 16:47:05 +0300

    update run.sh

--- online_inference/run.sh
+++ online_inference/run.sh
@@ -1,6 +1,6 @@
 if [ -z $MODEL_PATH ]
 then
-    export MODEL_PATH="model.pkl"
+    export MODEL_PATH="output_model.pkl"
 fi
 
 if [ -z $TRANSFORMER_PATH ]
@@ -10,14 +10,14 @@ fi
 
 if [[ ! -f $MODEL_PATH ]]
 then
-    gdown https://drive.google.com/.... --output=$MODEL_PATH
+    gdown https://drive.google.com/file/d/1dfo8_cH2ASH7hq1Xd20iysUwTP8G60oY/view?usp=sharing --output=$MODEL_PATH
 else
     echo "model already exists"
 fi
 
 if [[ ! -f $TRANSFORMER_PATH ]]
 then
-    gdown https://drive.google.com/... --output=$TRANSFORMER_PATH
+    gdown https://drive.google.com/file/d/1uRnWyfKfa7ZfB53B1eFqyJF2zclBD5gO/view?usp=sharing --output=$TRANSFORMER_PATH
 else 
     echo "transformer aldeady exists"
 fi
